58bee61cbc24f1fb5c1ff8ffdefb7bb1e6a5eeae
Florent Champigny <florent.champigny@idean.com>
2020-03-19 16:46:17 +0100

    added theme configurator

--- example/lib/lib/ThemeColorSelector.dart
+++ example/lib/lib/ThemeColorSelector.dart
@@ -3,25 +3,32 @@ import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 import 'color_selector.dart';
 
-class ThemeColorSelector extends StatelessWidget {
+class ThemeColorSelector extends StatefulWidget {
 
   final BuildContext customContext;
 
   ThemeColorSelector({this.customContext});
 
   @override
+  _ThemeColorSelectorState createState() => _ThemeColorSelectorState();
+}
+
+class _ThemeColorSelectorState extends State<ThemeColorSelector> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(4),
       color: Colors.black,
       child: ColorSelector(
-        color: NeumorphicTheme.baseColor(customContext ?? context),
+        color: NeumorphicTheme.baseColor(widget.customContext ?? context),
         onColorChanged: (color){
-          NeumorphicTheme.update(customContext ?? context, (current) =>
-              current.copyWith(
-                  baseColor: color
-              )
-          );
+          setState(() {
+            NeumorphicTheme.update(widget.customContext ?? context, (current) =>
+                current.copyWith(
+                    baseColor: color
+                )
+            );
+          });
         },
       ),
     );
